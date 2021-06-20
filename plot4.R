8cbcc676e189500489444cd7688b96eb42241bfd
nhilth <71610151+nhilth@users.noreply.github.com>
2021-06-21 00:43:57 +0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -11,6 +11,6 @@ with(df,plot(datetime, Voltage, type = "l",xlab = "datetime", ylab = "Voltage"))
 with( df, plot(datetime, Sub_metering_1, type = "l", xlab = "", ylab = "Enery sub metering"))
 with(df, lines(datetime, Sub_metering_2, type = "l", col = "red"))
 with(df, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
-legend("topright",legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1:1)
+legend("topright",legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1:1, bty =  "n") # lty: line type, bty: box type
 with(df,plot(datetime, Global_reactive_power, type = "l",xlab = "datetime", ylab = "Global_reactive_power"))
 dev.off()

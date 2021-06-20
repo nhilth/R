a7119668daebf98923f42bc8189cf75f5847cf38
nhilth <71610151+nhilth@users.noreply.github.com>
2021-06-21 00:46:41 +0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,9 +1,9 @@
 df <- read.table("C:/Users/sisu/Desktop/R/household_power_consumption.txt", header = TRUE, sep = ";") 
 head(df)
 dim(df)                                                                                    
-df <- subset(df, Date == "1/2/2007"|Date == "2/2/2007")                                     
+df <- subset(df, Date == "1/2/2007"|Date == "2/2/2007")                                               # "|" is OR                                  
 dim(df)                                                                                    
-png("plot2.png", width = 480, height = 480)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+png("plot2.png", width = 480, height = 480)         
+datetime <- strptime(paste(df$Date, df$Time, sep=" "), "%d/%m/%Y %H:%M:%S")                            # paste = + string; as.Date(); strptime = from_timestamp
 with(df,plot(datetime, Global_active_power, type = "l",xlab = "", ylab = "Global Active Power"))
 dev.off()
